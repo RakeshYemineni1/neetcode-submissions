1d0c96fe1a9bde420039d96242276d0f6d0232a2
Rakesh Yemineni <152261771+RakeshYemineni1@users.noreply.github.com>
2026-04-30 22:44:02 +0530

    Add: three-integer-sum - submission-0

---
+++ Data Structures & Algorithms/three-integer-sum/submission-0.java
@@ -0,0 +1,32 @@
+class Solution {
+    public List<List<Integer>> threeSum(int[] nums) {
+        List<List<Integer>> tSum = new ArrayList<>();
+        Arrays.sort(nums);
+        int n = nums.length;
+        Map<Integer, Integer> map = new HashMap<>();
+
+        for(int i = 0; i < n; i++){
+            map.put(nums[i], map.getOrDefault(nums[i], 0) + 1);
+        }
+
+        for(int i = 0; i < n; i++){
+            map.put(nums[i], map.get(nums[i]) - 1);
+
+            if(i > 0 && nums[i] == nums[i - 1]) continue;
+
+            for(int j = i + 1; j < n; j++){
+                map.put(nums[j], map.get(nums[j]) - 1);
+
+                if(j > i + 1 && nums[j] == nums[j - 1]) continue;
+
+                int k = 0 - (nums[i] + nums[j]);
+
+                if(map.getOrDefault(k, 0) > 0){
+                    tSum.add(Arrays.asList(nums[i], nums[j], k));
+                }
+            }
+            for(int j = i + 1; j < n; j++) map.put(nums[j], map.get(nums[j]) + 1);
+        }
+        return tSum;
+    }
+}
